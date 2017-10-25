9c033de20bba5b8e7475ce3f62db7cf89c928fad
ragecoder23 <ragecoder23@users.noreply.github.com>
2017-10-25 15:59:27 -0400

    Started V Stack

--- StackV.cpp
+++ StackV.cpp
@@ -1,1 +1,30 @@
 #include "StackV.h"
+
+void Stack::push(int x)
+{
+	data.push_back(x);
+}
+
+void Stack::pop()
+{
+	data.pop_back();
+}
+
+void clear()
+{
+	for (int i = 0; i < data.size();i++)
+	{
+		data.pop_back();
+	}
+	
+}
+
+int size()
+{
+	return data.size();
+}
+
+int top();
+{
+	return data.size()-1;
+}
